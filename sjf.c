40acf4a1b18a012561aa106149179138f7f6175d
philip <philipgeo73@gmail.com>
2019-08-07 02:28:48 +0530

    added arival time for sjf

--- sjf.c
+++ sjf.c
@@ -1,31 +1,26 @@
 #include<stdio.h>
 
 int main(){
-	int job_queue[100],count,i,time=0,j,tmp;
+	int job_queue[100][2],count,i,time=0,j,tmp,min,imin;
 	float wait_time=0,turn_time=0;
 	printf("enter the size followed by the cpu times required(max 100)\n");
 	scanf("%d",&count);
 
 	for(i=0;i<count;i++){
-		scanf("%d",job_queue+i);
+		scanf("%d %d",&job_queue[i][0],&job_queue[i][1]);
 	}
 
-	//sorting
-	for(i=1;i<count;i++)
-		for(j=i;j>0;j--)
-			if(job_queue[j]<job_queue[j-1]){
-				//swap
-				tmp=job_queue[j];
-				job_queue[j]=job_queue[j-1];
-				job_queue[j-1]=tmp;
-				//end of sawp
-			}
-	//end of sorting
-
 	for(i=0;i<count;i++){
-		wait_time += time;
-		time += job_queue[i];
-		turn_time += time;
+		min = 30000
+		imin = -1
+		for(j=0;j<count;j++){
+			if (job_queue[j][0]!=-1 && job_queue[j][0]<min ){
+				min = job_queue[j][0]
+				imin = j
+			}
+			
+		}
+		
 	}
 	printf("turn averge around time:%f\naverage waiting time:%f\n",(turn_time/count),(wait_time/count));
 
